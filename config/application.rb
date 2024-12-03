53da458418a7323dfb8e4f1d1a598b47ad206677
spacekimchi <spacekimchis@gmail.com>
2024-12-02 18:47:56 -0800

    upgrade to 7.2

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,12 @@ Bundler.require(*Rails.groups)
 module RailsSass
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     config.action_mailer.default_url_options = { host: "railssass.com" }
 
